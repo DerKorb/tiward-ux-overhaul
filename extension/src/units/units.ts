39eb5e99b891c1b61c5e242b56c90d8340426be8
Korbinian Sollner <ksollner@feinarbyte.de>
2023-11-07 22:50:53 +0100

    eco

--- extension/src/units/units.ts
+++ extension/src/units/units.ts
@@ -7,6 +7,7 @@ unitsComponentTemplate.innerHTML = `
             left: 50%;
             transform: translate(-50%, -50%);
         }
+
         #units {
             position: relative;
             background-color: rgba(0, 0, 0, 0.8);
@@ -35,6 +36,19 @@ unitsComponentTemplate.innerHTML = `
     <table id="units">
     </table>
 `;
+
+export const unitValue = {
+  Flagship: 8,
+  "War Sun": 12,
+  Dreadnought: 4,
+  Carrier: 3,
+  Cruiser: 2,
+  Destroyer: 1,
+  Fighter: 0.5,
+  PDS: 2,
+  Infantry: 0.5,
+  "Space Dock": 0,
+};
 
 class UnitsComponent extends HTMLElement {
   unitsContainer: HTMLElement | null;
@@ -78,13 +92,21 @@ class UnitsComponent extends HTMLElement {
       return acc;
     }, {} as Record<string, Record<string, number>>);
 
+    const unitValuesByPlayerColor = players.reduce((acc, player) => {
+      acc[player.color] = { ...defaultUnitCounts };
+      return acc;
+    }, {} as Record<string, Record<string, number>>);
+
     systems.forEach((system) => {
       system.units.forEach((unit) => {
         unitCountsByPlayerColor[unit.color][unit.name]++;
+        unitValuesByPlayerColor[unit.color][unit.name] += unitValue[unit.name];
       });
       system.planets.forEach((planet) => {
         planet.units.forEach((unit) => {
           unitCountsByPlayerColor[unit.color][unit.name]++;
+          unitValuesByPlayerColor[unit.color][unit.name] +=
+            unitValue[unit.name];
         });
       });
     });
@@ -94,6 +116,21 @@ class UnitsComponent extends HTMLElement {
       unitHeaderCell.textContent = unitName;
       headerRow.appendChild(unitHeaderCell);
     });
+
+    const headers = [
+      "Unit Value",
+      "Trade Goods",
+      "Influence",
+      "Resources",
+      "Technology",
+      "Command Counter",
+      "Score",
+    ];
+    for (const header of headers) {
+      const headerCell = document.createElement("td");
+      headerCell.textContent = header;
+      headerRow.appendChild(headerCell);
+    }
 
     players.forEach((player) => {
       // Create a row for each player with unit counts
@@ -113,6 +150,80 @@ class UnitsComponent extends HTMLElement {
           unitRow.appendChild(unitCountCell);
         }
       );
+
+      const unitValueCell = document.createElement("td");
+      unitValueCell.style.color = player.color;
+      unitValueCell.style.fontWeight = "bold";
+      unitValueCell.style.fontSize = "20px";
+      const unitValue = Object.values(
+        unitValuesByPlayerColor[player.color]
+      ).reduce((a, b) => a + b, 0);
+      unitValueCell.textContent = unitValue.toString();
+      unitRow.appendChild(unitValueCell);
+
+      const tradegoodsCell = document.createElement("td");
+      tradegoodsCell.style.color = player.color;
+      tradegoodsCell.style.fontWeight = "bold";
+      tradegoodsCell.style.fontSize = "20px";
+      tradegoodsCell.textContent = player.tradeGoods.toString();
+      unitRow.appendChild(tradegoodsCell);
+
+      const influenceCell = document.createElement("td");
+      influenceCell.style.color = player.color;
+      influenceCell.style.fontWeight = "bold";
+      influenceCell.style.fontSize = "20px";
+      const influenceValues = Object.values(
+        player.planetCards.map((planetCard) =>
+          planetCard.exhausted ? 0 : planetCard.influence
+        )
+      ).reduce((a, b) => a + b, 0);
+      influenceCell.textContent = influenceValues.toString();
+      unitRow.appendChild(influenceCell);
+
+      const resourcesCell = document.createElement("td");
+      resourcesCell.style.color = player.color;
+      resourcesCell.style.fontWeight = "bold";
+      resourcesCell.style.fontSize = "20px";
+      const resourceValues = Object.values(
+        player.planetCards.map((planetCard) =>
+          planetCard.exhausted ? 0 : planetCard.resources
+        )
+      ).reduce((a, b) => a + b, 0);
+      resourcesCell.textContent = resourceValues.toString();
+      unitRow.appendChild(resourcesCell);
+
+      const ccs =
+        (player.fleetPool + player.strategyPool + player.tacticPool) * 2;
+      const technologyValue = player.technology.length * 6;
+
+      const technologyCell = document.createElement("td");
+      technologyCell.style.color = player.color;
+      technologyCell.style.fontWeight = "bold";
+      technologyCell.style.fontSize = "20px";
+
+      technologyCell.textContent = technologyValue.toString();
+      unitRow.appendChild(technologyCell);
+
+      const ccCell = document.createElement("td");
+      ccCell.style.color = player.color;
+      ccCell.style.fontWeight = "bold";
+      ccCell.style.fontSize = "20px";
+      ccCell.textContent = ccs.toString();
+      unitRow.appendChild(ccCell);
+
+      const completeCell = document.createElement("td");
+      completeCell.style.color = player.color;
+      completeCell.style.fontWeight = "bold";
+      completeCell.style.fontSize = "20px";
+      completeCell.textContent = (
+        resourceValues +
+        (influenceValues * 2) / 3 +
+        unitValue +
+        technologyValue +
+        ccs +
+        player.tradeGoods
+      ).toFixed(0);
+      unitRow.appendChild(completeCell);
     });
   }
 }
